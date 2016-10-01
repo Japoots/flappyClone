0ae800f92d6c582070d4443c551835061fb01275
Japoots <thegreeny@hotmail.co.uk>
2016-10-01 14:16:05 +0100

    Add files via upload

--- flappyClone/Flappy/main.lua
+++ flappyClone/Flappy/main.lua
@@ -1,40 +1,32 @@
 
 function love.load()
-  
   background= love.graphics.newImage("sprites/bg.png")
-  backgroundQuad = love.graphics.newQuad(1,1,720/2,1280/2,720/2,1280/2)
-  
-  ground = love.graphics.newImage("sprites/ground.png")
-
-  
+  backgroundQuad = love.graphics.newQuad(1,1,720/2,1280/2,720/2,1280/2) 
+  ground = love.graphics.newImage("sprites/ground.png") 
   flappy = love.graphics.newImage("sprites/flappy.png")
-  flappyPosY = 300
-  
+  flappyPosY = 200
   pipe = love.graphics.newImage("sprites/pipe.png")
-  pipePosX = math.random(1,100)
-
+  pipePosX = 300
+  
+  
 end
 
 
 
 function love.draw()
   love.graphics.draw(background, backgroundQuad, 0, 0)
-  
-    love.graphics.draw(flappy, 125, flappyPosY)
+  love.graphics.draw(flappy, 125, flappyPosY)
   love.graphics.draw(ground, 0, 550)
-  for i=10,1,-1 do
-  love.graphics.draw(pipe, pipePosX + math.random(1,100), 400)
+  love.graphics.draw(pipe, pipePosX, 400)
+  if pipePosX < -80 then
+  pipePosX = 380
+  pipePosY = math.random(100,200)
+  love.graphics.draw(pipe, pipePosX, pipePosY)
   end
-  
-  
-  game_screen()
-  
-  
-  
+  game_screen() 
 end
 
 
-    
 function love.mousereleased(x, y, button)
    if button == 1 then
       jump()
@@ -42,12 +34,10 @@ function love.mousereleased(x, y, button)
 end
 
 function game_screen()
-  
-
-
   pipePosX = pipePosX - 1.5
   flappyPosY = flappyPosY + 2.5
   hitTest = CheckCollision(pipePosX, 500, 60, 150, 125, flappyPosY, 45, 45)
+  
   if(hitTest) then
     -- Do collision stuff
   end
@@ -62,5 +52,5 @@ function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
 end
 
 function jump()
-  flappyPosY = flappyPosY  - 100
+  flappyPosY = flappyPosY  - 55
 end
